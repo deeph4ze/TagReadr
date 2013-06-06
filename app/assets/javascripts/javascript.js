b78fdc8c07c9aedd872d295c2adaf64b3676d771
Kevin Lin <deeph4ze@gmail.com>
2013-06-06 16:47:43 -0700

    added weight sizing, fixed some padding issues

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -1,18 +1,52 @@
 
 $(document).ready(function(){
 	$('.tweet').each(function(){
-		size = ((Math.random()+1)*2)-1;
+		/*size = ((Math.random()+1)*2)-1;                    *****This was the code to random resize
 		$(this).css("font-size",(String(size)+"em"));
-            $(this).css("width", (String(size*150)+"px"));
+            $(this).css("width", (String(size*150)+"px"));*/
+            weight = $(this).data("weight");
+            if (weight < 10) {
+                  fontsize = "1em";
+                  width = "220px";
+            }
+            else if (weight < 25){
+                  fontsize = "1.25em";
+                  width = "460px";
+            }
+            else {
+                  fontsize = "1.5em";
+                  width ="700px";
+            }
+            $(this).css("font-size", fontsize);
+            $(this).css("width", width);
 	});
 	$('img').each(function(){
-		size = ((Math.random()+1)*200);
+		/*size = ((Math.random()+1)*200);                   ***Random Resize
 		$(this).css("height",(String(size)+"px"));
 		$(this).css("width",(String(size)+"px"));
 		$(this).parent().css("width", (String(size+5)+"px"));
-		$(this).parent().css("font-size", (String(size/240)+"em"))
+		$(this).parent().css("font-size", (String(size/240)+"em"))*/
+            weight = $(this).parent().data("weight");
+            if (weight < 10) {
+                  fontsize = "1em";
+                  width = 210;
+            }
+            else if (weight < 45){
+                  fontsize = "1.5em";
+                  width = 450;
+            }
+            else {
+                  fontsize = "1.5em";
+                  width = 690;
+            }
+            $(this).parent().css("width", String(width+10)+"px");
+            $(this).parent().css("font-size", fontsize);
+            $(this).css("width", (String(width))+"px");
 	});
+
+      //FUNCTIONS FOR ORDERING THE ITEMS
 	$('#contentcontainer').each(function(){
+            
             // get current ul
             var $div = $(this);
             // get array of list items in current ul
@@ -29,12 +63,9 @@ $(document).ready(function(){
                   return( isOddOrEven*isPosOrNeg );
             })
             // append list items to ul
-            .appendTo($div);            
+            .appendTo($div);          
       });
 
-      $(function(){
-      $('#contentcontainer').masonry({
-            columnWidth : 500
-            });
-      });
+      //MASONRY
+
 });
